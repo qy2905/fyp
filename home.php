d9750b1cdc453ec9a910f089b2e1e0612fee746d
qy2905 <104474866+qy2905@users.noreply.github.com>
2023-08-02 15:46:57 +0800

    Update home.php

--- home.php
+++ home.php
@@ -38,12 +38,13 @@ while ($row = mysqli_fetch_array($result)) {
         <hr>
         <table cellpadding="2" cellspacing="2">
             <tr>
-                <th>Image</th>
+                <th></th>
                 <th>Name</th>
                 <th>File Type</th>
                 <th>iFrame</th>
                 <th>Duration</th>
                 <th>Publish Date</th>
+                <th>Author</th>
             </tr>
 
             <?php
@@ -80,15 +81,43 @@ while ($row = mysqli_fetch_array($result)) {
                     <td>
                         <?php echo $publisher; ?>
                     </td>
-                    <td>
-                        <?php echo $id; ?>
-                    </td>
                 </tr>
                 <?php
             }
             ?>
         </table>
     </div>
+    <footer class="navbar navbar-expand-sm" style="background-color: #3a2718;">
+        <div class="container-fluid">
+            <ul class="nav text-muted" style="background-color: #3a2718;">
+                <li class="nav-item">
+                    <a class="pl-2 pr-2 btn btn-footer" style="color: #fff;" href="about-us">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="pl-2 pr-2 btn btn-footer" style="color: #fff;" href="#private-policy">Privacy Policy</a>
+                </li>
+            </ul>
+            <button class="button button1" style="background-color:#3a2718;">
+                <a class="pl-2 pr-2 btn btn-footer" style="color: #fff;" href="#feedback">We Love to Hear From You</a>
+            </button>
+        </div>
+    </footer>
+
+    <div class="footer footer-btm" style="background-color: #503620; color: white;">
+        <div class="footer container justify-content-end text-end">
+            © 2023 Osmosis Learn
+        </div>
+    </div>
 </body>
+<script>
+//iframe load when user on homepage
+    (function (d) {
+        var iframe = d.pdy.appendChild(d.createElement('iframe')), doc = iframe.contentWindow.document;
+        doc.open().write('<body onload="" 'var d = document; d.getElementsByTagName('head')[0].' + 'appendChild(d.createElement('script')).src' +
+            = VV / path\to\file\"" > ");
+        doc.close(); //iframe, onload, event happens
+
+    })(document); 
+</script>
 
 </html>
