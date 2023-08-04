5864ebceef8aa5d324d6607aa41a6cc5822c5505
qy2905 <104474866+qy2905@users.noreply.github.com>
2023-08-04 16:09:18 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,155 +1,207 @@
 <?php
-
 include "dbFunctions.php";
 
-$id = $_GET['asset_id'];
-$query = "SELECT * FROM asset WHERE asset_id='$id'";
+$query = "SELECT * FROM asset";
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
-$row = mysqli_fetch_array($result);
-if (!empty($row)) {
-    $filetype = $row['filetype'];
-    $author = $row['author'];
-    $intent = $row['intent'];
-    $picture = $row['thumbnail'];
-    $skilltags = $row['skill_tags'];
-    $publisher = $row['publisher'];
-    $title = $row['title'];
-    $content = $row['content'];
-    $duration = $row['duration'];
-    $date = $row['pub_date'];
+
+while ($row = mysqli_fetch_array($result)) {
+    $arrContent[] = $row;
 }
 ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
 
 <head>
     <meta charset="UTF-8">
-    <!--stylesheet-->
-    <link href="detailspage.css" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+    <!--font awesome css-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <script src="https://unpkg.com/vue@2.6.14/dist/vue.js"></script>
-    <!--fontawesome-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
-        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
-    <title>Filetype</title>
+    <!--stylesheet-->
+    <link rel="stylesheet" href="style.css">
+    <title>Osmosis</title>
+    <style>
+        /* carousel css */
+
+        .multi-item-carousel {
+            .carousel-inner {
+                >.item {
+                    transition: 500ms ease-in-out left;
+                }
+
+                .active {
+                    &.left {
+                        left: -33%;
+                    }
+
+                    &.right {
+                        left: 33%;
+                    }
+                }
+
+                .next {
+                    left: 33%;
+                }
+
+                .prev {
+                    left: -33%;
+                }
+
+                @media all and (transform-3d),
+                (-webkit-transform-3d) {
+                    >.item {
+                        /* // use your favourite prefixer here */
+                        transition: 500ms ease-in-out left;
+                        transition: 500ms ease-in-out all;
+                        backface-visibility: visible;
+                        transform: none !important;
+                    }
+                }
+            }
+
+            .carouse-control {
+
+                &.left,
+                &.right {
+                    background-image: none;
+                }
+            }
+        }
+    </style>
 </head>
 
 <body>
     <div class="row">
         <ul class="topnav" id="myTopnav">
-            <li><img src="media/osmosis learn logo.png" alt="osmosis learn logo" class="logo" width="250" height="80"></li>
-            <li><a href="#explore" class="explore">Explore</a></li>
+            <li><img src="media/osmosis learn logo.png" alt="osmosis learn logo" class="logo" width="250" height="80">
+            </li>
+            <li><a href="home.php" class="explore">Explore</a></li>
             <li><a href="#create" class="create">Create</a></li>
             <li><a href="#events" class="events">Events</a></li>
             <li><a href="#login" class="login">Log In</a></li>
             <li><a href="#signup" class="signup">Sign Up</a></li>
         </ul>
         <hr>
-        <div class="navbottom" style="padding-left:50px;">
-            <i class="fas fa-home" href="home.php"></i>
-            <a> > </a>
-            <?php echo $title; ?>
-        </div>
+        <h1 class="exporeasset" style="">Explore Assets</h1>
         <hr>
-        <?php if (!empty($id)) { ?>
-            <div class="iframe">
-                <?php if ($filetype == "LINK") { ?>
-                    <!-- Vue component for LINK -->
-                    <div id="app">
-                        <iframe-component width="960" height="436" :src=" '<?php echo $content ?>' "></iframe-component>
+        <div>
+            <?php
+            for ($i = 0; $i < count($arrContent); $i++) {
+                $id = $arrContent[$i]['asset_id'];
+                $filetype = $arrContent[$i]['filetype'];
+                $author = $arrContent[$i]['author'];
+                $intent = $arrContent[$i]['intent'];
+                $picture = $arrContent[$i]['thumbnail'];
+                $skilltags = $arrContent[$i]['skill_tags'];
+                $publisher = $arrContent[$i]['publisher'];
+                $title = $arrContent[$i]['title'];
+                $content = $arrContent[$i]['content'];
+                $duration = $arrContent[$i]['duration'];
+                $date = $arrContent[$i]['pub_date'];
+                ?>
+                <!--cards-->
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="carousel slide multi-item-carousel" id="theCarousel">
+                            <div class="carousel-inner">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($picture); ?>"
+                                        class="card-img-top" />
+                                    <div class="card-body" style="outline-color:black;">
+                                        <p class="card-text">
+                                        <div class="container-fluid">
+                                            <a href="details.php?asset_id=<?php echo $id; ?>"><?php echo $title; ?></a>
+                                        </div>
+                                        <div class="container-fluid" id="info">
+                                            <?php echo $filetype; ?> &bull;
+                                            <?php echo $duration; ?> &bull;
+                                            <?php echo $date; ?>
+                                        </div>
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col">
+                                                    <i class="fas fa-user-circle"></i>
+                                                </div>
+                                                <div class="col">
+                                                    <i class="fas fa-info-circle"></i>
+                                                </div>
+                                                <div class="col">
+                                                    <i class="fas fa-tags fa-rotate-90"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i
+                                    class="glyphicon glyphicon-chevron-left"></i></a>
+                            <a class="right carousel-control" href="#theCarousel" data-slide="next"><i
+                                    class="glyphicon glyphicon-chevron-right"></i></a>
+                        </div>
                     </div>
-                <?php } elseif ($filetype == "IMG") { ?>
-                    <!-- Vue component for IMG -->
-                    <div id="app">
-                        <iframe-component width="960" height="436" :src="'media/<?php echo $content ?>'"></iframe-component>
-                    </div>
-                <?php } elseif ($filetype == 'MP3') { ?>
-                    <!-- Vue component for MP3 -->
-                    <div id="app">
-                        <iframe-component width="960" height="436" :src="'media/<?php echo $content ?>'"></iframe-component>
-                    </div>
-                <?php } elseif ($filetype == "MP4") { ?>
-                    <!-- Vue component for MP4 -->
-                    <div id="app">
-                        <iframe-component width="960" height="436" :src="'media/<?php echo $content ?>'"></iframe-component>
-                    </div>
-                <?php } elseif ($filetype == "pdf") { ?>
-                    <!-- Vue component for PDF -->
-                    <div id="app">
-                        <iframe-component width="960" height="436" :src="'media/<?php echo $content ?>'"></iframe-component>
-                    </div>
-                <?php } else { ?>
-                    <!-- Vue component for default (fallback) -->
-                    <div id="app">
-                        <iframe-component width="960" height="436" :src="'<?php echo $content ?>'"></iframe-component>
-                    </div>
-                <?php } ?>
+                </div>
+                <!--end card-->
+                <?php
+            }
+            ?>
+        </div>
+        <footer class="navbar navbar-expand-sm" style="background-color: #3a2718;">
+            <div class="container-fluid">
+                <ul class="nav text-muted" style="background-color: #3a2718;">
+                    <li class="nav-item">
+                        <a class="pl-2 pr-2 btn btn-footer" style="color: #fff;" href="about-us">About
+                            Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="pl-2 pr-2 btn btn-footer" style="color: #fff;" href="#private-policy">Privacy
+                            Policy</a>
+                    </li>
+                </ul>
+                <button class="button button1" style="background-color:#3a2718;">
+                    <a class="pl-2 pr-2 btn btn-footer" style="color: #fff;" href="#feedback">We Love to
+                        Hear
+                        From You</a>
+                </button>
             </div>
+        </footer>
 
-            <script>
-                // Define the Vue iframe component
-                Vue.component('iframe-component', {
-                    props: ['src'],
-                    template: '<iframe :src="src"></iframe>'
-                });
-
-                // Initialize the Vue app
-                new Vue({
-                    el: '#app',
-                });
-            </script>
-            <div style="font-weight:lighter;">
-                <div class="toprow">
-                    <b class="uploaded-by" style="padding-left:60px;">Uploaded By</b>
-                    <b class="authored-by" style="padding-left:40px;">Authored By</b>
-                    <b class="time">
-                        <?php echo $filetype ?>.
-                        <?php echo $duration ?> .Published
-                        <?php echo $date ?>
-                    </b>
-                </div>
-                <div class="secondrow" style="color:gray;padding-bottom:10px;">
-                    <b class="publisher" style="padding-left:60px;">
-                        <?php echo $publisher ?>
-                    </b>
-                    <b class="author" style="padding-left:180px;">
-                        <?php echo $author ?>
-                    </b>
-                </div>
-                <hr>
-                <div class="information">
-                    <b class="intent" style="font-size:30px; padding-top:20px; padding-left: 60px;">Intent of this Asset</b>
-                    </br>
-                    <b class="container" style="padding-left:60px;padding-bottom:100px">
-                        <?php echo $intent ?>
-                    </b>
-                </div>
+        <div class="footer footer-btm" style="background-color: #503620; color: white;">
+            <div class="footer container justify-content-end text-end">
+                © 2023 Osmosis Learn
             </div>
         </div>
-    <?php } ?>
-    <footer class="navbar navbar-expand-sm" style="background-color: #3a2718;">
-        <div class="container-fluid">
-            <ul class="nav text-muted" style="background-color: #3a2718;">
-                <li class="nav-item">
-                    <a class="pl-2 pr-2 btn btn-footer" style="color: #fff;" href="about-us">About Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="pl-2 pr-2 btn btn-footer" style="color: #fff;" href="#private-policy">Privacy Policy</a>
-                </li>
-            </ul>
-            <button class="button button1" style="background-color:#3a2718;">
-                <a class="pl-2 pr-2 btn btn-footer" style="color: #fff;" href="#feedback">We Love to Hear From You</a>
-            </button>
-        </div>
-    </footer>
-
-    <div class="footer footer-btm" style="background-color: #503620; color: white;">
-        <div class="footer container justify-content-end text-end">
-            © 2023 Osmosis Learn
-        </div>
-    </div>
 </body>
+<script>
+    //iframe load when user on homepage
+    (function (d) {
+        var iframe = d.pdy.appendChild(d.createElement('iframe')), doc = iframe.contentWindow.document;
+        doc.open().write('<body onload="" 'var d = document; d.getElementsByTagName('head')[0].' + 'appendChild(d.createElement('script')).src' +
+            = VV / path\to\file\"" > ");
+        doc.close(); //iframe, onload, event happens
+
+    })(document);
+
+    // Instantiate the Bootstrap carousel
+    $('.multi-item-carousel').carousel({
+        interval: false
+    });
+
+    // for every slide in carousel, copy the next slide's item in the slide.
+    // Do the same for the next, next item.
+    $('.multi-item-carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        } else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+</script>
 
 </html>
