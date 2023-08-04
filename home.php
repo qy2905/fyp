a9954653bc87c87843d6a402f47ff39ed4d5cba1
qy2905 <104474866+qy2905@users.noreply.github.com>
2023-08-04 15:54:03 +0800

    updated home page but carousel not working

--- home.php
+++ home.php
@@ -1,26 +1,39 @@
 <?php
+
 include "dbFunctions.php";
 
-$query = "SELECT * FROM asset";
+$id = $_GET['asset_id'];
+$query = "SELECT * FROM asset WHERE asset_id='$id'";
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
-
-while ($row = mysqli_fetch_array($result)) {
-    $arrContent[] = $row;
+$row = mysqli_fetch_array($result);
+if (!empty($row)) {
+    $filetype = $row['filetype'];
+    $author = $row['author'];
+    $intent = $row['intent'];
+    $picture = $row['thumbnail'];
+    $skilltags = $row['skill_tags'];
+    $publisher = $row['publisher'];
+    $title = $row['title'];
+    $content = $row['content'];
+    $duration = $row['duration'];
+    $date = $row['pub_date'];
 }
 ?>
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="UTF-8">
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <!--stylesheet-->
+    <link href="detailspage.css" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
-    <!--font awesome css-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <!--stylesheet-->
-    <link rel="stylesheet" href="style.css">
-    <title>FYP prototype</title>
+    <script src="https://unpkg.com/vue@2.6.14/dist/vue.js"></script>
+    <!--fontawesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <title>Filetype</title>
 </head>
 
 <body>
@@ -34,59 +47,88 @@ while ($row = mysqli_fetch_array($result)) {
             <li><a href="#signup" class="signup">Sign Up</a></li>
         </ul>
         <hr>
-        <h1>Explore Assets</h1>
+        <div class="navbottom" style="padding-left:50px;">
+            <i class="fas fa-home" href="home.php"></i>
+            <a> > </a>
+            <?php echo $title; ?>
+        </div>
         <hr>
-        <table cellpadding="2" cellspacing="2">
-            <tr>
-                <th></th>
-                <th>Name</th>
-                <th>File Type</th>
-                <th>iFrame</th>
-                <th>Duration</th>
-                <th>Publish Date</th>
-                <th>Author</th>
-            </tr>
+        <?php if (!empty($id)) { ?>
+            <div class="iframe">
+                <?php if ($filetype == "LINK") { ?>
+                    <!-- Vue component for LINK -->
+                    <div id="app">
+                        <iframe-component width="960" height="436" :src=" '<?php echo $content ?>' "></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == "IMG") { ?>
+                    <!-- Vue component for IMG -->
+                    <div id="app">
+                        <iframe-component width="960" height="436" :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == 'MP3') { ?>
+                    <!-- Vue component for MP3 -->
+                    <div id="app">
+                        <iframe-component width="960" height="436" :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == "MP4") { ?>
+                    <!-- Vue component for MP4 -->
+                    <div id="app">
+                        <iframe-component width="960" height="436" :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == "pdf") { ?>
+                    <!-- Vue component for PDF -->
+                    <div id="app">
+                        <iframe-component width="960" height="436" :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } else { ?>
+                    <!-- Vue component for default (fallback) -->
+                    <div id="app">
+                        <iframe-component width="960" height="436" :src="'<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } ?>
+            </div>
 
-            <?php
-            for ($i = 0; $i < count($arrContent); $i++) {
-                $id = $arrContent[$i]['asset_id'];
-                $filetype = $arrContent[$i]['filetype'];
-                $author = $arrContent[$i]['author'];
-                $intent = $arrContent[$i]['intent'];
-                $picture = $arrContent[$i]['thumbnail'];
-                $skilltags = $arrContent[$i]['skill_tags'];
-                $publisher = $arrContent[$i]['publisher'];
-                $title = $arrContent[$i]['title'];
-                $content = $arrContent[$i]['content'];
-                $duration = $arrContent[$i]['duration'];
-                $date = $arrContent[$i]['pub_date'];
-                ?>
+            <script>
+                // Define the Vue iframe component
+                Vue.component('iframe-component', {
+                    props: ['src'],
+                    template: '<iframe :src="src"></iframe>'
+                });
 
-                <tr>
-                    <td><img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($picture); ?>" />
-                    </td>
-                    <td>
-                        <?php echo $title; ?>
-                    </td>
-                    <td>
-                        <?php echo $filetype; ?>
-                    </td>
-                    <td><a href="details.php?asset_id=<?php echo $id; ?>">Click Here</a></td>
-                    <td>
-                        <?php echo $duration; ?>
-                    </td>
-                    <td>
-                        <?php echo $date; ?>
-                    </td>
-                    <td>
-                        <?php echo $publisher; ?>
-                    </td>
-                </tr>
-                <?php
-            }
-            ?>
-        </table>
-    </div>
+                // Initialize the Vue app
+                new Vue({
+                    el: '#app',
+                });
+            </script>
+            <div style="font-weight:lighter;">
+                <div class="toprow">
+                    <b class="uploaded-by" style="padding-left:60px;">Uploaded By</b>
+                    <b class="authored-by" style="padding-left:40px;">Authored By</b>
+                    <b class="time">
+                        <?php echo $filetype ?>.
+                        <?php echo $duration ?> .Published
+                        <?php echo $date ?>
+                    </b>
+                </div>
+                <div class="secondrow" style="color:gray;padding-bottom:10px;">
+                    <b class="publisher" style="padding-left:60px;">
+                        <?php echo $publisher ?>
+                    </b>
+                    <b class="author" style="padding-left:180px;">
+                        <?php echo $author ?>
+                    </b>
+                </div>
+                <hr>
+                <div class="information">
+                    <b class="intent" style="font-size:30px; padding-top:20px; padding-left: 60px;">Intent of this Asset</b>
+                    </br>
+                    <b class="container" style="padding-left:60px;padding-bottom:100px">
+                        <?php echo $intent ?>
+                    </b>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
     <footer class="navbar navbar-expand-sm" style="background-color: #3a2718;">
         <div class="container-fluid">
             <ul class="nav text-muted" style="background-color: #3a2718;">
@@ -109,15 +151,5 @@ while ($row = mysqli_fetch_array($result)) {
         </div>
     </div>
 </body>
-<script>
-//iframe load when user on homepage
-    (function (d) {
-        var iframe = d.pdy.appendChild(d.createElement('iframe')), doc = iframe.contentWindow.document;
-        doc.open().write('<body onload="" 'var d = document; d.getElementsByTagName('head')[0].' + 'appendChild(d.createElement('script')).src' +
-            = VV / path\to\file\"" > ");
-        doc.close(); //iframe, onload, event happens
-
-    })(document); 
-</script>
 
 </html>
